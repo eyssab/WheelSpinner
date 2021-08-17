b7b9df6602b8212abfd6980408e8395d71073187
Eyssa Benhida <eyssab@gmail.com>
2021-08-17 00:19:55 -0400

    added text and rotated it about origin

--- WheelSpinner.java
+++ WheelSpinner.java
@@ -30,11 +30,17 @@ public class WheelSpinner extends ApplicationAdapter {
 	private PolygonSprite polySprite;
 	private float multiplier = 3.6f;
 	private Array<Color> colorsArray;
+	private BitmapFont font;
+	private Array<String> text;
+	public SpriteBatch spriteBatch;
+	private Array<Matrix4> mxArray;
+	private Array<TextureRegion> textureRegion;
+	private String prev = "                ";
+
 
 	//Mutable UI Variables
 	private float pies;
 	private FloatArray sliceSize;
-	private Array<TextureRegion> textureRegion;
 
 	@Override
 	public void create () {
@@ -46,29 +52,30 @@ public class WheelSpinner extends ApplicationAdapter {
 		batch = new SpriteBatch();
 
 		//UI TABLE # of Pies and percentage of wheel per pie
-		pies = 7;
-		FloatArray percentages = new FloatArray(new float[]{20,10,5,5,20,15,25});
-		colorsArray = new Array<Color>(new Color[]{Color.MAROON, Color.CHARTREUSE, Color.BROWN, Color.ORANGE, Color.CORAL, Color.CYAN, Color.GOLD});
+		pies = 8;
+		FloatArray percentages = new FloatArray(new float[]{20, 10, 5, 5, 20, 15, 20, 5});
+		colorsArray = new Array<Color>(new Color[]{Color.MAROON, Color.CHARTREUSE, Color.BROWN, Color.ORANGE, Color.CORAL, Color.CYAN, Color.GOLD, Color.LIME});
+		text = new Array<String>(new String[]{prev + "Hola", prev +"Lemon", prev +"Lemon", prev +"Lemon", prev +"Lemon", prev +"Lemon", prev +"Lemon", prev +"Lemon"});
 
 		vertexArr = new Array<FloatArray>();
 
 		//degrees per pie
 		sliceSize = new FloatArray();
 
-		for(int i=0;i<pies;i++) {
+		for (int i = 0; i < pies; i++) {
 			sliceSize.add(percentages.get(i) * multiplier);
 		}
 
-		for(int i=0;i<pies;i++) {
-			FloatArray piesVertices = new FloatArray(new float[]{vertexCalc(sliceSize.get(i)).x, vertexCalc(sliceSize.get(i)).y, 400,400,vertexCalc(0).x,vertexCalc(0).y, vertexCalc(sliceSize.get(i)*0.15f).x,vertexCalc(sliceSize.get(i)*0.15f).y, vertexCalc(sliceSize.get(i)/4).x, vertexCalc(sliceSize.get(i)/4).y, vertexCalc(sliceSize.get(i)/2).x, vertexCalc(sliceSize.get(i)/2).y, vertexCalc(sliceSize.get(i)*0.7f).x,vertexCalc(sliceSize.get(i)*0.7f).y, vertexCalc(sliceSize.get(i)*0.8f).x,vertexCalc(sliceSize.get(i)*0.8f).y});
+		for (int i = 0; i < pies; i++) {
+			FloatArray piesVertices = new FloatArray(new float[]{vertexCalc(sliceSize.get(i)).x, vertexCalc(sliceSize.get(i)).y, 400, 400, vertexCalc(0).x, vertexCalc(0).y, vertexCalc(sliceSize.get(i) * 0.15f).x, vertexCalc(sliceSize.get(i) * 0.15f).y, vertexCalc(sliceSize.get(i) / 4).x, vertexCalc(sliceSize.get(i) / 4).y, vertexCalc(sliceSize.get(i) / 2).x, vertexCalc(sliceSize.get(i) / 2).y, vertexCalc(sliceSize.get(i) * 0.7f).x, vertexCalc(sliceSize.get(i) * 0.7f).y, vertexCalc(sliceSize.get(i) * 0.8f).x, vertexCalc(sliceSize.get(i) * 0.8f).y});
 			vertexArr.add(piesVertices);
 		}
 
 		polyBatch = new PolygonSpriteBatch();
 
 		textureRegion = new Array<TextureRegion>();
-		for(int i =0;i<pies;i++) {
-			Pixmap pix = new Pixmap(1,1,Pixmap.Format.RGBA8888);
+		for (int i = 0; i < pies; i++) {
+			Pixmap pix = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
 			pix.setColor(colorsArray.get(i));
 			pix.fill();
 
@@ -82,15 +89,34 @@ public class WheelSpinner extends ApplicationAdapter {
 
 		polysArray = new Array<PolygonSprite>();
 
-		for(int i = 0; i<vertexArr.size;i++) {
+		for (int i = 0; i < vertexArr.size; i++) {
 			ShortArray triangleIndices = triangulator.computeTriangles(vertexArr.get(i));
 			PolygonRegion polyReg = new PolygonRegion(textureRegion.get(i), vertexArr.get(i).toArray(), triangleIndices.toArray());
 			polySprite = new PolygonSprite(polyReg);
 			polysArray.add(polySprite);
-			polysArray.get(i).translate(0,-20);
+			polysArray.get(i).translate(0, -20);
 		}
 
-		polyRotater(polysArray, sliceSize);
+		polyRotate(polysArray, sliceSize);
+
+		font = new BitmapFont(Gdx.files.internal("default.fnt"));
+		font.getData().setScale(1.5f);
+		mxArray = new Array<Matrix4>();
+
+		float current = 0f;
+		FloatArray textRotation = new FloatArray();
+		for(int i = 1;i<pies;i++){
+			textRotation.add(polysArray.get(i).getRotation()/2);
+		}
+
+		for(int i=0;i<pies;i++){
+			Matrix4 mx4Font = new Matrix4();
+			mx4Font.rotate(new Vector3(0, 0, 1), (sliceSize.get(i))/2+current);
+			current +=sliceSize.get(i);
+			System.out.println(current);
+			mx4Font.trn(392, 386, 0);
+			mxArray.add(mx4Font);
+		}
 	}
 
 	@Override
@@ -123,7 +149,17 @@ public class WheelSpinner extends ApplicationAdapter {
 		}
 		if (spinTime < random(10,15) && spin) {
 			decrement *= 0.995;
-			rotate1(polysArray, decrement);
+			wheelSpin(polysArray, decrement);
+			textSpin(mxArray,decrement);
+		}
+
+		spriteBatch = new SpriteBatch();
+
+		for(int i=0;i<pies;i++) {
+			spriteBatch.setTransformMatrix(mxArray.get(i));
+			spriteBatch.begin();
+			font.draw(spriteBatch, text.get(i), 0, 0);
+			spriteBatch.end();
 		}
 	}
 
@@ -136,13 +172,20 @@ public class WheelSpinner extends ApplicationAdapter {
 		return coords;
 	}
 
-	public void rotate1(Array<PolygonSprite> arr, float rotation){
+	public void wheelSpin(Array<PolygonSprite> arr, float rotation){
 		for (int i = 0; i < arr.size; i++) {
 			arr.get(i).rotate(rotation);
 		}
 	}
 
-	private void polyRotater(Array<PolygonSprite> arr, FloatArray rotations){
+	public void textSpin(Array<Matrix4> arr, float rotation){
+		for (int i = 0; i < arr.size; i++) {
+			arr.get(i).rotate(new Vector3(400, 400, 1),rotation);
+			mxArray.get(i).trn(392, 386, 0);
+		}
+	}
+
+	private void polyRotate(Array<PolygonSprite> arr, FloatArray rotations){
 		float current = 0;
 		for (int i = 0; i < pies; i++) {
 			polysArray.get(i).rotate(current);
